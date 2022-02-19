368330322e896976b661a4468bcbada82c731aec
namaramulla <namara_mulla@persistent.com>
2022-02-19 15:14:40 +0530

    Added function header

--- math.py
+++ math.py
@@ -1,12 +1,16 @@
+#Add implementation
 def add(x,y):
     return x+y
 
+#Subtract implementation
 def subtract(x,y):
     return x-y             
 
+#Multiply implementation
 def multiply(x,y):
     return x*y
 
+#Divide implementation
 def division(x,y):
     return x*x
 
